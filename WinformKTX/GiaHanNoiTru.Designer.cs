2bf7f2f3559ebc2fda1540daacb0d1da06ba58cc
trongnguyentrongnguyen123 <trongnguyentrongnguyen123@gmail.com>
2025-02-15 09:37:16 +0700

    Trong
    lần 2

--- WinformKTX/GiaHanNoiTru.Designer.cs
+++ WinformKTX/GiaHanNoiTru.Designer.cs
@@ -28,32 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GiaHanNoiTru));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            txtThoigiangiahan = new Guna.UI2.WinForms.Guna2ComboBox();
+            btnTracuunoitru = new Guna.UI2.WinForms.Guna2Button();
+            txtMasinhvien = new Guna.UI2.WinForms.Guna2TextBox();
+            btnDangkygiahan = new Guna.UI2.WinForms.Guna2Button();
+            label3 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            txtTienphong = new Guna.UI2.WinForms.Guna2TextBox();
             label1 = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // txtThoigiangiahan
+            // 
+            txtThoigiangiahan.BackColor = Color.Transparent;
+            txtThoigiangiahan.CustomizableEdges = customizableEdges1;
+            txtThoigiangiahan.DrawMode = DrawMode.OwnerDrawFixed;
+            txtThoigiangiahan.DropDownStyle = ComboBoxStyle.DropDownList;
+            txtThoigiangiahan.FocusedColor = Color.FromArgb(94, 148, 255);
+            txtThoigiangiahan.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtThoigiangiahan.Font = new Font("Segoe UI", 10F);
+            txtThoigiangiahan.ForeColor = Color.Black;
+            txtThoigiangiahan.ItemHeight = 30;
+            txtThoigiangiahan.Items.AddRange(new object[] { "3" });
+            txtThoigiangiahan.Location = new Point(593, 273);
+            txtThoigiangiahan.Name = "txtThoigiangiahan";
+            txtThoigiangiahan.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            txtThoigiangiahan.Size = new Size(300, 36);
+            txtThoigiangiahan.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            txtThoigiangiahan.TabIndex = 159;
+            // 
+            // btnTracuunoitru
+            // 
+            btnTracuunoitru.BorderRadius = 10;
+            btnTracuunoitru.BorderThickness = 1;
+            btnTracuunoitru.CheckedState.BorderColor = Color.FromArgb(255, 192, 192);
+            btnTracuunoitru.CheckedState.FillColor = Color.White;
+            btnTracuunoitru.CustomizableEdges = customizableEdges3;
+            btnTracuunoitru.DisabledState.BorderColor = Color.DarkGray;
+            btnTracuunoitru.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnTracuunoitru.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnTracuunoitru.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnTracuunoitru.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnTracuunoitru.ForeColor = Color.White;
+            btnTracuunoitru.Image = (Image)resources.GetObject("btnTracuunoitru.Image");
+            btnTracuunoitru.ImageSize = new Size(35, 35);
+            btnTracuunoitru.Location = new Point(960, 188);
+            btnTracuunoitru.Name = "btnTracuunoitru";
+            btnTracuunoitru.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            btnTracuunoitru.Size = new Size(165, 56);
+            btnTracuunoitru.TabIndex = 158;
+            btnTracuunoitru.Text = "TRA CỨU";
+            btnTracuunoitru.Click += btnTracuunoitru_Click;
+            // 
+            // txtMasinhvien
+            // 
+            txtMasinhvien.CustomizableEdges = customizableEdges5;
+            txtMasinhvien.DefaultText = "";
+            txtMasinhvien.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtMasinhvien.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtMasinhvien.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtMasinhvien.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtMasinhvien.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtMasinhvien.Font = new Font("Segoe UI", 9F);
+            txtMasinhvien.ForeColor = Color.Black;
+            txtMasinhvien.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtMasinhvien.Location = new Point(593, 188);
+            txtMasinhvien.Margin = new Padding(3, 4, 3, 4);
+            txtMasinhvien.Name = "txtMasinhvien";
+            txtMasinhvien.PasswordChar = '\0';
+            txtMasinhvien.PlaceholderText = "Nhập mã sinh viên";
+            txtMasinhvien.SelectedText = "";
+            txtMasinhvien.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            txtMasinhvien.Size = new Size(300, 45);
+            txtMasinhvien.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            txtMasinhvien.TabIndex = 157;
+            // 
+            // btnDangkygiahan
+            // 
+            btnDangkygiahan.BorderRadius = 10;
+            btnDangkygiahan.BorderThickness = 1;
+            btnDangkygiahan.CheckedState.BorderColor = Color.FromArgb(255, 192, 192);
+            btnDangkygiahan.CheckedState.FillColor = Color.White;
+            btnDangkygiahan.CustomizableEdges = customizableEdges7;
+            btnDangkygiahan.DisabledState.BorderColor = Color.DarkGray;
+            btnDangkygiahan.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnDangkygiahan.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnDangkygiahan.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnDangkygiahan.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold);
+            btnDangkygiahan.ForeColor = Color.White;
+            btnDangkygiahan.Image = (Image)resources.GetObject("btnDangkygiahan.Image");
+            btnDangkygiahan.ImageSize = new Size(35, 35);
+            btnDangkygiahan.Location = new Point(960, 265);
+            btnDangkygiahan.Name = "btnDangkygiahan";
+            btnDangkygiahan.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            btnDangkygiahan.Size = new Size(165, 56);
+            btnDangkygiahan.TabIndex = 156;
+            btnDangkygiahan.Text = "GIA HẠN";
+            btnDangkygiahan.Click += btnDangkygiahan_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(370, 284);
+            label3.Name = "label3";
+            label3.Size = new Size(191, 25);
+            label3.TabIndex = 155;
+            label3.Text = "THỜI GIAN GIA HẠN";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(370, 197);
+            label2.Name = "label2";
+            label2.Size = new Size(167, 25);
+            label2.TabIndex = 154;
+            label2.Text = "MÃ SỐ SINH VIÊN";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top;
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(593, 75);
+            label4.Name = "label4";
+            label4.Size = new Size(314, 46);
+            label4.TabIndex = 153;
+            label4.Text = "GIA HẠN NỘI TRÚ";
+            // 
+            // txtTienphong
+            // 
+            txtTienphong.CustomizableEdges = customizableEdges9;
+            txtTienphong.DefaultText = "";
+            txtTienphong.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtTienphong.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtTienphong.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtTienphong.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtTienphong.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtTienphong.Font = new Font("Segoe UI", 9F);
+            txtTienphong.ForeColor = Color.Black;
+            txtTienphong.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtTienphong.Location = new Point(593, 349);
+            txtTienphong.Margin = new Padding(3, 4, 3, 4);
+            txtTienphong.Name = "txtTienphong";
+            txtTienphong.PasswordChar = '\0';
+            txtTienphong.PlaceholderText = "";
+            txtTienphong.SelectedText = "";
+            txtTienphong.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            txtTienphong.Size = new Size(300, 45);
+            txtTienphong.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            txtTienphong.TabIndex = 162;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(403, 232);
+            label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(370, 358);
             label1.Name = "label1";
-            label1.Size = new Size(105, 20);
-            label1.TabIndex = 0;
-            label1.Text = "gia han noi tru";
+            label1.Size = new Size(124, 25);
+            label1.TabIndex = 161;
+            label1.Text = "TIỀN PHÒNG";
             // 
-            // GiaHanNoiTru
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 430);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1488, 505);
+            dataGridView1.TabIndex = 163;
+            // 
+            // GiaHanNoiTru1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.LightGray;
+            ClientSize = new Size(1512, 947);
+            Controls.Add(dataGridView1);
+            Controls.Add(txtTienphong);
             Controls.Add(label1);
-            Name = "GiaHanNoiTru";
-            Text = "GiaHanNoiTru";
+            Controls.Add(txtThoigiangiahan);
+            Controls.Add(btnTracuunoitru);
+            Controls.Add(txtMasinhvien);
+            Controls.Add(btnDangkygiahan);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label4);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "GiaHanNoiTru1";
+            Text = "GiaHanNoiTru1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
+        private Guna.UI2.WinForms.Guna2ComboBox txtThoigiangiahan;
+        private Guna.UI2.WinForms.Guna2Button btnTracuunoitru;
+        private Guna.UI2.WinForms.Guna2TextBox txtMasinhvien;
+        private Guna.UI2.WinForms.Guna2Button btnDangkygiahan;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private Guna.UI2.WinForms.Guna2TextBox txtTienphong;
         private Label label1;
+        private DataGridView dataGridView1;
     }
 }
